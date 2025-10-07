0454b942e4f7f4de2dc81d7b1d3e61690d16449c
Matt Calayo <matthewcalayo@gmail.com>
2025-10-07 18:11:14 -0400

    first commit

---
+++ apps/expo/metro.config.js
@@ -0,0 +1,27 @@
+// Learn more: https://docs.expo.dev/guides/monorepos/
+const path = require("node:path");
+const { getDefaultConfig } = require("expo/metro-config");
+const { FileStore } = require("metro-cache");
+const { withNativeWind } = require("nativewind/metro");
+
+module.exports = withTurborepoManagedCache(
+  withNativeWind(getDefaultConfig(__dirname), {
+    input: "./src/styles.css",
+    configPath: "./tailwind.config.ts",
+  }),
+);
+
+/**
+ * Move the Metro cache to the `.cache/metro` folder.
+ * If you have any environment variables, you can configure Turborepo to invalidate it when needed.
+ *
+ * @see https://turborepo.com/docs/reference/configuration#env
+ * @param {import('expo/metro-config').MetroConfig} config
+ * @returns {import('expo/metro-config').MetroConfig}
+ */
+function withTurborepoManagedCache(config) {
+  config.cacheStores = [
+    new FileStore({ root: path.join(__dirname, ".cache/metro") }),
+  ];
+  return config;
+}
